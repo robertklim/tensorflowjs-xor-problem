840d3a3fdd3830bf34f61b95b96e832c2099ee2f
Robert Klim <kontakt.robert.klim@gmail.com>
2018-12-16 17:15:03 +0100

    Define tf model with 2 inputs, 2 hidden nodes and 1 output

--- js/sketch.js
+++ js/sketch.js
@@ -1,5 +1,53 @@
+let model;
+
+// data for XOR
+// [0, 1] -> [1]
+// [1, 0] -> [1]
+// [0, 0] -> [0]
+// [1, 1] -> [0]
+let training_data = [
+    {
+        inputs: [0, 1],
+        targets: [1]
+    },
+    {
+        inputs: [1, 0],
+        targets: [1]
+    },
+    {
+        inputs: [0, 0],
+        targets: [0]
+    },
+    {
+        inputs: [1, 1],
+        targets: [0]
+    },
+];
+
 function setup() {
     createCanvas(400, 400);
+
+    // define tf model:
+    // 2 inputs
+    // 2 hidden nodes
+    // 1 output
+    
+    model = tf.sequential();
+    
+    let hidden = tf.layers.dense({
+        inputShape: [2],
+        units: 2,
+        activation: 'sigmoid'
+    });
+    
+    let output = tf.layers.dense({
+        units: 1,
+        activation: 'sigmoid'
+    });
+    
+    model.add(hidden);
+    model.add(output);
+
 }
 
 function draw() {
